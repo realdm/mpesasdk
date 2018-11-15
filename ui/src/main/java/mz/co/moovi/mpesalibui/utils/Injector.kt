3fbc752dcbc672f993bf2556a416ddde39152fef
realdm <dario.mungoi@shopify.com>
2018-11-15 18:58:49 -0500

    Add Configurable Endpoint Configurable
    This will add the endpoint url as configurable property in the MpesaSdk
    class.
    With this, the clients using the sdk will be able to specify which
    endpoint url they will want to use when configuring the SDK.

--- ui/src/main/java/mz/co/moovi/mpesalibui/utils/Injector.kt
+++ ui/src/main/java/mz/co/moovi/mpesalibui/utils/Injector.kt
@@ -4,14 +4,14 @@ import mz.co.moovi.mpesalib.api.MpesaMpesaServiceImpl
 import mz.co.moovi.mpesalib.api.MpesaRepositoryImpl
 import mz.co.moovi.mpesalib.api.MpesaService
 import mz.co.moovi.mpesalib.config.MpesaConfig
-import mz.co.moovi.mpesalibui.MpesaSdk.SANDBOX_BASE_URL
 import mz.co.moovi.mpesalibui.MpesaSdk.apiKey
+import mz.co.moovi.mpesalibui.MpesaSdk.endpointUrl
 import mz.co.moovi.mpesalibui.MpesaSdk.publicKey
 
 object Injector {
 
     fun mPesaService(): MpesaService {
-        val config = MpesaConfig(baseUrl = SANDBOX_BASE_URL, apiKey = apiKey, publicKey = publicKey)
+        val config = MpesaConfig(baseUrl = endpointUrl, apiKey = apiKey, publicKey = publicKey)
         val repository = MpesaRepositoryImpl(config)
         return MpesaMpesaServiceImpl(repository)
     }
