dfcfd6665235f5e741d915d89af1fa0d61a756ee
realdm <dario.mungoi@shopify.com>
2018-11-15 19:39:12 -0500

    Add Public Key to MpesaSdk Configuration
    This will add the public key as a property to be set when configuring
    the MpesaSdk.

--- ui/src/main/java/mz/co/moovi/mpesalibui/MpesaSdk.kt
+++ ui/src/main/java/mz/co/moovi/mpesalibui/MpesaSdk.kt
@@ -11,7 +11,7 @@ object MpesaSdk {
 
     const val ARG_RESULT_TRANSACTION_ID = "transaction_id"
     const val ARG_RESULT_CONVERSATION_ID = "conversation_id"
-    
+
     const val ARG_SERVICE_PROVIDER_NAME = "arg_provider_name"
     const val ARG_SERVICE_PROVIDER_CODE = "arg_provider_code"
     const val ARG_TRANSACTION_AMOUNT = "arg_transaction_amount"
@@ -43,8 +43,13 @@ object MpesaSdk {
     val endpointUrl: String
         get() = _endpointUrl
 
+    private lateinit var _publicKey: String
+    val publicKey: String
+        get() = _publicKey
+
     fun init(
         apiKey: String,
+        publicKey: String,
         serviceProviderName: String,
         serviceProviderCode: String,
         serviceProviderLogoUrl: String = "",
@@ -56,14 +61,13 @@ object MpesaSdk {
             throw IllegalArgumentException("SDK is already initialized")
         }
         _apiKey = apiKey
+        _publicKey = publicKey
         _endpointUrl = endpointUrl
         _serviceProviderCode = serviceProviderCode
         _serviceProviderName = serviceProviderName
         _serviceProviderLogoUrl = serviceProviderLogoUrl
         initialized = true
     }
-
-    val publicKey = """MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAmptSWqV7cGUUJJhUBxsMLonux24u+FoTlrb+4Kgc6092JIszmI1QUoMohaDDXSVueXx6IXwYGsjjWY32HGXj1iQhkALXfObJ4DqXn5h6E8y5/xQYNAyd5bpN5Z8r892B6toGzZQVB7qtebH4apDjmvTi5FGZVjVYxalyyQkj4uQbbRQjgCkubSi45Xl4CGtLqZztsKssWz3mcKncgTnq3DHGYYEYiKq0xIj100LGbnvNz20Sgqmw/cH+Bua4GJsWYLEqf/h/yiMgiBbxFxsnwZl0im5vXDlwKPw+QnO2fscDhxZFAwV06bgG0oEoWm9FnjMsfvwm0rUNYFlZ+TOtCEhmhtFp+Tsx9jPCuOd5h2emGdSKD8A6jtwhNa7oQ8RtLEEqwAn44orENa1ibOkxMiiiFpmmJkwgZPOG/zMCjXIrrhDWTDUOZaPx/lEQoInJoE2i43VN/HTGCCw8dKQAwg0jsEXau5ixD0GUothqvuX3B9taoeoFAIvUPEq35YulprMM7ThdKodSHvhnwKG82dCsodRwY428kg2xM/UjiTENog4B6zzZfPhMxFlOSFX4MnrqkAS+8Jamhy1GgoHkEMrsT5+/ofjCx0HjKbT5NuA2V/lmzgJLl3jIERadLzuTYnKGWxVJcGLkWXlEPYLbiaKzbJb2sYxt+Kt5OxQqC1MCAwEAAQ=="""
 
     fun pay(activity: Activity, requestCode: Int, amount: String, transactionReference: String) {
         if (!initialized) {
