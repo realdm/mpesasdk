f77e8aba17b9fac085211f8eb7335755ca733cd9
Dario Mungoi <dariomungoi@Kuroku-MPB.local>
2022-01-11 10:39:45 -0500

    update to java 11

--- ui/src/main/java/mz/co/moovi/mpesalibui/payment/c2b/components/ReadyToPay.kt
+++ ui/src/main/java/mz/co/moovi/mpesalibui/payment/c2b/components/ReadyToPay.kt
@@ -1,6 +1,5 @@
 package mz.co.moovi.mpesalibui.payment.c2b.components
 
-import abcdar.io.ruca.ui.theme.MpesaSdkUiTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.focusable
@@ -8,7 +7,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +20,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
@@ -159,29 +160,5 @@ fun ReadyToPay(
             )
         }
 
-    }
-}
-
-@Preview
-@Composable
-fun ReadyToPayPreview() {
-    MpesaSdkUiTheme {
-        Surface {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                ReadyToPay(
-                    viewState = C2BPaymentViewState.ReadyToPay(
-                        amount = "20",
-                        phoneNumber = "849873879",
-                        payButtonEnabled = true,
-                        providerShortCode = "7777",
-                        providerName = "Vodacom MZ",
-                        providerLogo = ""
-                    ),
-                    onEditNumber = {},
-                    onPay = {
-                    }
-                )
-            }
-        }
     }
 }
